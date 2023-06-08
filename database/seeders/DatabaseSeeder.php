83374cc26aabdbcb534724d5394aa2d8bc070e3c
Emerson Ovídio <emersonovidiosantos@gmail.com>
2023-06-08 15:35:41 -0300

    create model and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::insert('insert into products (description, quantity, values, due date) values (?,?,?,?)', array('Arroz', 10, 10, '2023-12-01'));
+        DB::insert('insert into products (description, quantity, values, due date) values (?,?,?,?)', array('Feijão', 8, 8.00, '2023-08-24'));
+        DB::insert('insert into products (description, quantity, values, due date) values (?,?,?,?)', array('Óleo', 12, 5.99, '2023-10-01'));
+        DB::insert('insert into products (description, quantity, values, due date) values (?,?,?,?)', array('Farinha', 20, 3.5, '2023-09-12'));
     }
 }
